efaad4be837c92bf0281f819f8076ea28c4d7854
nepa27 <nepalek@mail.ru>
2024-12-10 16:47:02 +0300

    Add parse sizes into WB

--- wb.py
+++ wb.py
@@ -5,6 +5,10 @@ import requests
 def get_page():
     request = requests.get('http://192.168.1.133/wb/')
 
+    return request
+
+
+def parse_price(request):
     soup = BeautifulSoup(request.text, 'lxml')
     green_price = soup.find('span', class_='product-line__price-wallet').text.strip().rstrip(' ₽  ')
     main_price = soup.find('b', class_='product-line__price-now wallet').text.strip().rstrip(' ₽  ')
@@ -13,4 +17,19 @@ def get_page():
     print(f'Цена без WB кошелька: {main_price}')
 
 
-get_page()
+def parse_size(request):
+    soup = BeautifulSoup(request.text, 'lxml')
+    list_sizes = soup.find(
+        'ul', class_='sizes-list visible'
+    ).find_all('label', class_='j-size sizes-list__button')
+
+    print('Наличие размеров:')
+    for i, el in enumerate(list_sizes):
+        size = el.find('span', class_='sizes-list__size').text
+        print(f'{i + 1}. {size}')
+
+
+req = get_page()
+
+parse_price(req)
+parse_size(req)
