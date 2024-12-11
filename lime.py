1e2e45984331507ddf8d1ae7c382d754f5f0810f
nepa27 <nepalek@mail.ru>
2024-12-11 06:25:48 +0300

    Add headers in request; Add constants

--- lime.py
+++ lime.py
@@ -1,10 +1,32 @@
+from datetime import datetime
+import os
+
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 import requests
 
+from constants import FILES_PATH
 
-def get_page():
-    request = requests.get('http://192.168.1.133/lime/')
+def get_page(url):
+    now_date = datetime.now().strftime('%d_%m_%Y|%H_%M_%S')
+    user_agent = UserAgent().random
+    headers = {
+        'user-agent': user_agent
+    }
+    request = requests.get(
+        url,
+        headers=headers
+    )
 
+    if not os.path.exists(FILES_PATH):
+        os.mkdir(FILES_PATH)
+
+    with open(f'{FILES_PATH}/{now_date}.html', 'w') as file:
+        file.write(request.text)
+
+    return request
+
+def parse_price(request):
     soup = BeautifulSoup(request.text, 'lxml')
     # TODO: Проверить страницы со скидками, узнать название класса
     # green_price = soup.find('span', class_='s4m_27 ms3_27').text.rstrip(' ₽')
@@ -14,4 +36,6 @@ def get_page():
     print(f'Цена: {main_price}')
 
 
-get_page()
+req = get_page('https://lime-shop.com/ru_ru/product/21901_9983_688-sero_koricnevyi')
+
+#parse_price(req)
