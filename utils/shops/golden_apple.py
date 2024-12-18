df02899ddb1668992cba4051e850d31922fb9509
nepa27 <nepalek@mail.ru>
2024-12-18 11:47:22 +0300

    Change tags and flags for parsing in GOLDEN APPLE

--- utils/shops/golden_apple.py
+++ utils/shops/golden_apple.py
@@ -1,18 +1,32 @@
+import os
+
 from bs4 import BeautifulSoup
-import requests
+from dotenv import load_dotenv
+
+
+load_dotenv('.env')
+key_price = os.getenv('FLAG_PRICE_GA')
+tag_price_volume = os.getenv('TAG_PRICE_VOLUME_GA')
+tag_volume = os.getenv('TAG_VOLUME_GA')
 
 
 def parse_golden_apple(response):
     """Парсит цену на товар в GOLDEN APPLE."""
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    green_price = soup.find(
-        'div',
-        class_='uLqLO baA1b'
-    ).find('div').text.strip().rstrip('\n₽\n ')
-    main_price = soup.find(
-        'div',
-        class_='vTuTT _8denb'
-    ).text.strip().rstrip('\n₽\n ')
+    soup = BeautifulSoup(response, 'lxml')
+    data = soup.find(tag_price_volume).find(tag_volume)
+
+    raw_prices = data.find_next_sibling()
+    raw_prices = raw_prices.text.split(key_price)
+
+    green_price = raw_prices[0].strip().rstrip()
+    main_price = raw_prices[1].strip().rstrip()
 
     print(f'Цена с картой: {green_price}')
+    print(f'Цена без карты: {main_price}')
+
+
+with open('golden.html', 'r') as f:
+    file = f.read()
+
+parse_golden_apple(file)
