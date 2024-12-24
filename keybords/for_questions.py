b11c26e9dc32daf39c15c1e0eef1a98f427358f5
nepa27 <nepalek@mail.ru>
2024-12-24 15:41:01 +0300

    Add base keyboards

--- keybords/for_questions.py
+++ keybords/for_questions.py
@@ -48,3 +48,71 @@ def shops_kb() -> InlineKeyboardMarkup:
     )
     kb.adjust(1)
     return kb.as_markup(resize_keyboard=True)
+
+
+def tracking_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text='Последние изменения',
+        callback_data='last_changes'
+    )
+    kb.button(
+        text='Статистика',
+        callback_data='statistic'
+    )
+    kb.button(
+        text='Уведомления',
+        callback_data='notification'
+    )
+    kb.button(
+        text='Удалить',
+        callback_data='delete_thing'
+    )
+    kb.button(
+        text='Назад',
+        callback_data='back'
+    )
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def choose_notifications() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text='Каждые 3 часа',
+        callback_data='every_third_hours_notification'
+    )
+    kb.button(
+        text='Раз в сутки',
+        callback_data='one_time_day_notification'
+    )
+    kb.button(
+        text='При изменении цены',
+        callback_data='price_change_notification'
+    )
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def button_back_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text='Назад',
+        callback_data='back'
+    )
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
+
+
+def question_on_delete_thing_kb() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(
+        text='Да',
+        callback_data='yes_delete'
+    )
+    kb.button(
+        text='Нет',
+        callback_data='no_delete'
+    )
+    kb.adjust(1)
+    return kb.as_markup(resize_keyboard=True)
