86689365864aee288494117fafb80ccbfe2a803d
nepa27 <nepalek@mail.ru>
2025-02-17 15:19:56 +0300

    Some change in methods of work with BD

--- db/db.py
+++ db/db.py
@@ -1,4 +1,7 @@
 import asyncio
+import datetime
+
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     AsyncSession,
@@ -15,8 +18,7 @@ async def init_db():
         await conn.run_sync(Base.metadata.create_all)
 
 
-# Срабатывает когда пользователь нажимает /start и запускает бота
-async def add_user():
+async def add_user(user_id: int):
     async_session = async_sessionmaker(
         engine,
         class_=AsyncSession,
@@ -24,27 +26,22 @@ async def add_user():
     )
 
     async with async_session() as session:
-        user = UsersTable(tg_id=123456, username='Test')
-        session.add(user)
-        await session.commit()
+        response = select(UsersTable).where(
+            UsersTable.tg_id == user_id,
 
+        )
+        result = await session.execute(response)
+        user = result.scalar()
 
-async def check_thing():
-    async_session = async_sessionmaker(
-        engine,
-        class_=AsyncSession,
-        expire_on_commit=False
-    )
-
-    async with async_session() as session:
-        thing = await session.get(ThingsTable, 2)
-        if thing:
-            return 'Товар уже отслеживается!'
+        if user:
+            return user
         else:
-            return 'Товар не существует! Нужно добавить!'
+            user = UsersTable(tg_id=user_id)
+            session.add(user)
+            await session.commit()
 
 
-async def add_data_on_thing():
+async def check_thing(url: str, user_id: int):
     async_session = async_sessionmaker(
         engine,
         class_=AsyncSession,
@@ -52,23 +49,55 @@ async def add_data_on_thing():
     )
 
     async with async_session() as session:
-        thing = ThingsTable(url='http://example2.com', id_user=1)
-        price = PricesOfThingsTable(price=99, thing=thing)
+        response = select(ThingsTable).where(
+            and_(
+                ThingsTable.url == url,
+                ThingsTable.id_user == user_id,
+            )
+        )
+        result = await session.execute(response)
+        thing = result.scalar()
+
+        return thing if thing else None
+
+
+async def add_data_on_thing(url: str, user_id: int, data: list):
+    async_session = async_sessionmaker(
+        engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with async_session() as session:
+        date_today = datetime.datetime.now()
+        thing = ThingsTable(
+            url=url,
+            id_user=user_id,
+            thing_name=data[0]
+        )
+        price = PricesOfThingsTable(
+            price=data[1],
+            id_thing=thing.id,
+            added_at=date_today
+        )
         session.add_all((thing, price))
         await session.commit()
 
 
-async def add_new_price():
+async def add_new_price(thing_id: int, new_price: int):
     async_session = async_sessionmaker(
         engine,
         class_=AsyncSession,
         expire_on_commit=False
     )
 
-    async with async_session() as session:
-        thing = await session.get(ThingsTable, 2)
+    async with (async_session() as session):
+        response = select(ThingsTable).where(ThingsTable.id == thing_id)
+        result = await session.execute(response)
+        thing = result.scalar()
+
         if thing:
-            price = PricesOfThingsTable(price=92, thing=thing)
+            price = PricesOfThingsTable(price=new_price, thing=thing)
             session.add(price)
             await session.commit()
         else:
@@ -86,22 +115,40 @@ async def check_price():
     )
 
     async with async_session() as session:
-        thing = await session.query(PricesOfThingsTable).where(
-            PricesOfThingsTable.id_thing == think_id,
-            PricesOfThingsTable.price == new_price
+        response = select(PricesOfThingsTable).where(
+            and_(
+                PricesOfThingsTable.id_thing == think_id,
+                PricesOfThingsTable.price == new_price
+            )
         )
-        if thing:
-            return 'Цена не изменилась'
-        else:
-            return 'Цена изменилась'
+        result = await session.execute(response)
+        price = result.scalar()
+
+        return 'Цена не изменилась' if price else 'Цена изменилась'
 
 
-async def main():
-    await init_db()  # Инициализация базы данных
-    # await add_data()  # Добавление данных
-    await add_new_price()
+async def get_list_things(user_id: int):
+    async_session = async_sessionmaker(
+        engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
 
-
-# Запуск основной функции
-if __name__ == "__main__":
-    asyncio.run(main())
+    async with async_session() as session:
+        response = select(ThingsTable).where(
+            ThingsTable.id_user == user_id,
+        )
+        result = await session.execute(response)
+        data = result.scalars().all()
+        return data if data else None
+#
+#
+# async def main():
+#     await init_db()  # Инициализация базы данных
+#     # await add_data_on_thing()  # Добавление данных
+#     # await check_price()
+#     # await check_thing('http://example2.com', 1)
+#     await get_list_things(123)
+# # Запуск основной функции
+# if __name__ == "__main__":
+#     asyncio.run(main())
