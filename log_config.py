0d05c81780db185e98eb9c6175aa6eba940b471d
nepa27 <nepalek@mail.ru>
2025-02-21 16:28:00 +0300

    Change logger -> async_logger

--- log_config.py
+++ log_config.py
@@ -1,19 +1,21 @@
 import logging
-from logging.handlers import RotatingFileHandler
 import sys
+
+import asyncio
+from asyncio_logger import AsyncLogger, StreamHandlerAsync, FileHandlerAsync
 
 
 log_format = (
     '%(asctime)s - [%(levelname)s] -  %(name)s - '
     '(%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
 )
-logger = logging.getLogger(__name__)
+logger = AsyncLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-stream_handler = logging.StreamHandler(stream=sys.stdout)
+stream_handler = StreamHandlerAsync(stream=sys.stdout)
 stream_handler.setFormatter(logging.Formatter(log_format))
 
-file_handler = RotatingFileHandler(
+file_handler = FileHandlerAsync(
     f'{__file__}.log',
     maxBytes=5 * 1024 * 1024,
     backupCount=5,
