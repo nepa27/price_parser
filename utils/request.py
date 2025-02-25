d0be2d5fea72e352c9ab91b64c74da4ea1705d56
nepa27 <nepalek@mail.ru>
2025-02-25 10:47:48 +0300

    Add request for ozon

--- utils/request.py
+++ utils/request.py
@@ -1,9 +1,9 @@
-import os
 import time
 from datetime import datetime
 
 from fake_useragent import UserAgent
 from selenium import webdriver
+import undetected_chromedriver as uc
 
 from constants import FILES_PATH
 
@@ -25,11 +25,41 @@ def get_page(url):
 
         html = driver.page_source
 
-        if not os.path.exists(FILES_PATH):
-            os.mkdir(FILES_PATH)
+        # if not os.path.exists(FILES_PATH):
+        #     os.mkdir(FILES_PATH)
 
-        with open(f'{FILES_PATH}/{now_date}.html', 'w') as file:
-            file.write(html)
+        # with open(f'{FILES_PATH}/{now_date}.html', 'w') as file:
+        #     file.write(html)
+    except BaseException as er:
+        print(f'Возникла ошибка: {er}')
+    finally:
+        driver.quit()
+
+    return html
+
+
+def get_page_ozon(url):
+    html = None
+    now_date = datetime.now().strftime('%d_%m_%Y|%H_%M_%S')
+
+    options = uc.ChromeOptions()
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-extensions')
+
+    driver = uc.Chrome(
+        options=options,
+    )
+
+    try:
+        driver.get(url)
+        time.sleep(5)
+        html = driver.page_source
+
+        # if not os.path.exists(FILES_PATH):
+        #     os.mkdir(FILES_PATH)
+        #
+        # with open(f'{FILES_PATH}/{now_date}.html', 'w') as file:
+        #     file.write(html)
     except BaseException as er:
         print(f'Возникла ошибка: {er}')
     finally:
