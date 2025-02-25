3a1ada68b432c6cc61d124804d14cd13a71c471a
nepa27 <nepalek@mail.ru>
2025-02-25 10:48:33 +0300

    Add 'if' for ozon

--- utils/main.py
+++ utils/main.py
@@ -1,14 +1,18 @@
-from utils.shops import golden_apple, lime, wb
-from utils.request import get_page
+from utils.shops import golden_apple, lime, wb, ozon
+from utils.request import get_page, get_page_ozon
 
 
 async def choose_shop(url):
-    data = get_page(url)
-    if 'wb' in url or 'wildberries' in url:
-        return wb.parse_wb(data)
-    elif 'goldapple' in url:
-        return golden_apple.parse_golden_apple(data)
-    elif 'lime-shop' in url:
-        return lime.parse_lime(data)
+    if 'ozon' in url:
+        data = get_page_ozon(url)
+        return ozon.parse_ozon(data)
     else:
-        return None
+        data = get_page(url)
+        if 'wb' in url or 'wildberries' in url:
+            return wb.parse_wb(data)
+        elif 'goldapple' in url:
+            return golden_apple.parse_golden_apple(data)
+        elif 'lime-shop' in url:
+            return lime.parse_lime(data)
+        else:
+            return None
